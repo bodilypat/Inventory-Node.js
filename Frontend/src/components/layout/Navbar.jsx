e7782814b7001dce188a85786776f363e1662397
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-03-09 11:03:48 -0700

    Create Navbar.jsx

---
+++ Frontend/src/components/layout/Navbar.jsx
@@ -0,0 +1,16 @@
+//src/components/layout/Navbar.jsx
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <h3>Inventory Management System</h3>
+
+            <div className="nav-right">
+                <span>Admin</span>
+                <button className="logout-btn">Logout</button>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
+
