e188ae95eb1769b891f102f8421ddc68310ccbcc
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-03-09 10:58:19 -0700

    Add files via upload

---
+++ Frontend/src/utils/tokenStorage.js
@@ -0,0 +1,16 @@
+//src/utils/tokenStorage.js 
+
+const TOKEN_KEY = 'ims_token';
+
+export const setToken = (token) => {
+    localStorage.setItem(TOKEN_KEY, token);
+};
+
+export const getToken = () => {
+    return localStorage.getItem(TOKEN_KEY);
+};
+
+export const removeToken = () => {
+    localStorage.removeItem(TOKEN_KEY);
+};
+
