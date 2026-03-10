bd689bcac7b9e1cd0b32004e1993bd4c6485ba19
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-03-10 01:20:04 -0700

    Add files via upload

--- Frontend/src/components/ui/index.js
+++ Frontend/src/components/ui/index.js
@@ -1,3 +1,28 @@
-//src/components/ui/Select/index.js 
+//src/components/ui/index.js 
 
-export { default } from "./Select";
+export { default as Button } from './Button';
+export { default as Input } from './Input';
+export { default as Modal } from './Select';
+export { default as Card } from './CheckBox';
+
+export { Table, TableHeader, TableRow, TableCell } from './Table';
+
+export {
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+} from "./Modal";
+
+export {
+    Card,
+    ModalHeader,
+    ModalBody,
+    CardFooter,
+} from "./Card";
+
+export { default as Badge } from './Badge';
+export { default as Spinner } from './Spinner';
+export { default as Loader} from './Loader';
+export { default as Pagination } from './Pagination';
+
