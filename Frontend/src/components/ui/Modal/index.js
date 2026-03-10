62cb9f2264fd6bcd60e5b5b72f1ac8426406c484
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-03-09 22:31:29 -0700

    Create index.js

---
+++ Frontend/src/components/ui/Modal/index.js
@@ -0,0 +1,7 @@
+//src/components/ui/Modal/index.js 
+
+export { default as Modal } from './Modal';
+export { default as ModalHeader } from './ModalHeader';
+export { default as ModalBody } from './ModalBody';
+export { default as ModalFooter } from './ModalFooter';
+
