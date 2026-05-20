b0ad80e575e7e666c91170e736646fcf727538ec
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-05-19 21:51:55 -0700

    Add files via upload

---
+++ Backend/src/database/index.js
@@ -0,0 +1,6 @@
+//src/database/index.js 
+import connectDB from "../config/db.js";
+
+export {
+    connectDB
+};
--- Backend/src/database/seed.js
+++ Backend/src/database/seed.js
@@ -1,1 +1,56 @@
-//src/database/seed.js
+//src/database/seed.js 
+import mongoose from "mongoose";
+
+import connectDB from "../config/db.js";
+
+import User from "../modules/user.model.js";
+import Category from "../modules/categories/category.model.js";
+import Setting from "../modules/settings/settings.model.js";
+
+import { hashPassword } from "../modules/users/user.model.js";
+import { ROLES } from "../constants/roles.js";
+
+const seedDatabase = async () => {
+    try {
+        await connectDB();
+
+        console.log(` Starting database seed...`);
+
+        /* CLEAR EXISTING DATA */
+        await User.deleteMany();
+        await Category.deleteMany();
+        await Settings.deleteMany();
+
+        /* CREATE DEFAULT ADMIN */
+        const adminPassword = await hashPassword(Admin@psb);
+
+        const admin = await User.create({
+            name: "System Admin",
+            email: "admin@psbinventory.com",
+            password: adminPassword,
+            role: ROLES.ADMIN
+        });
+
+        console.log(" Admin user created");
+
+        /* DEFAULT CATEGORIES */
+        await Settings.create({
+            companyName: "Inventory Management System",
+            currency: "USD",
+            lowStockThreshold: 10 
+        });
+
+        console.log(" Settings seeds ")
+
+        console.log(" Database seed completed");
+
+        process.exit(0);
+    } catch (error) {
+        console.log(" Seed failed:", error.message);
+
+        process.exit(1);
+    }
+};
+
+seedDatabase();
+
